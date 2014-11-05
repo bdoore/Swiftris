83eb1490ab343f45bac1349c9036adb9a37000f4
Brian Doore <doore.b@gmail.com>
2014-11-05 12:04:44 -0600

    Initial Commit

---
+++ Sloth DropTests/Sloth_DropTests.swift
@@ -0,0 +1,36 @@
+//
+//  Sloth_DropTests.swift
+//  Sloth DropTests
+//
+//  Created by Brian Doore on 11/5/14.
+//  Copyright (c) 2014 Brian Doore. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Sloth_DropTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
